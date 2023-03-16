5f6cc21148d0d652aaae1ba9974e2091eb1e6959
it21271328 <99182048+it21271328@users.noreply.github.com>
2023-03-16 14:32:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/electricitysaver/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.electricitysaver
+
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Hide the status bar
+        //window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        // If you want to make the action bar (title bar) disappear as well, add this code:
+        supportActionBar?.hide()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.navigationBarColor = Color.parseColor("#133B5C") // Replace with your desired color
+        }
+    }
+}
