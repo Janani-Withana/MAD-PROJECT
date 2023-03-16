775ff41f58bf78479f4b775e5e0af0cf75a63379
IT21786464 <IT21786464@my.sliit.lk>
2023-03-16 23:46:04 +0530

    addCustomitem created

---
+++ app/src/main/java/com/example/electricitysaver/AddCustomItem.kt
@@ -0,0 +1,11 @@
+package com.example.electricitysaver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddCustomItem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_custom_item)
+    }
+}
