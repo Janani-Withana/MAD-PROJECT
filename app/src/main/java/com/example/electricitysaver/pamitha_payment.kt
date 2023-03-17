f6cea4518b9d1be5499f5b5856baf31ffaa659ff
it21271328 <99182048+it21271328@users.noreply.github.com>
2023-03-17 19:13:18 +0530

    payment designed

--- app/src/main/java/com/example/electricitysaver/pamitha_payment.kt
+++ app/src/main/java/com/example/electricitysaver/pamitha_payment.kt
@@ -1,5 +1,7 @@
 package com.example.electricitysaver
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,12 @@ class pamitha_payment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pamitha_payment2)
+
+        supportActionBar?.hide()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.navigationBarColor =
+                Color.parseColor("#133B5C") // Replace with your desired color
+        }
+
     }
 }
