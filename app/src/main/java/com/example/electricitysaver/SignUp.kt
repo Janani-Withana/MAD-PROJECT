4aff0ea80a98275d5f0b0413ea7f7309013bebbf
it21271328 <99182048+it21271328@users.noreply.github.com>
2023-03-17 12:50:11 +0530

    strings added

--- app/src/main/java/com/example/electricitysaver/SignUp.kt
+++ app/src/main/java/com/example/electricitysaver/SignUp.kt
@@ -1,5 +1,7 @@
 package com.example.electricitysaver
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,10 @@ class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup_bottomsheet_layout)
+        supportActionBar?.hide()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.navigationBarColor = Color.parseColor("#133B5C") // Replace with your desired color
+        }
     }
 }
