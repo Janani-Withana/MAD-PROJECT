5fab41a2b20e6192b555a9db9cd05d4c85945335
it21271328 <99182048+it21271328@users.noreply.github.com>
2023-03-17 12:21:38 +0530

    SignUp page designed

---
+++ app/src/main/java/com/example/electricitysaver/SignUp.kt
@@ -0,0 +1,11 @@
+package com.example.electricitysaver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup_bottomsheet_layout)
+    }
+}
