fa7483cfe1ba7ad7b0a5d9a01e7851bf35df7a69
it21271328 <99182048+it21271328@users.noreply.github.com>
2023-03-17 10:56:38 +0530

    SignUp page designed

--- app/src/main/java/com/example/electricitysaver/MainActivity.kt
+++ app/src/main/java/com/example/electricitysaver/MainActivity.kt
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,27 +18,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button5 = findViewById<Button>(R.id.tactivity)
-        button5.setOnClickListener {
-            val intent = Intent(this, User_addItems::class.java)
-            startActivity(intent)
-        }
+                // Hide the status bar
+                //window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
-
-
-
-
-
-
-
-        // Hide the status bar
-        //window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-
-        // If you want to make the action bar (title bar) disappear as well, add this code:
+                // If you want to make the action bar (title bar) disappear as well, add this code:
         supportActionBar?.hide()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.navigationBarColor = Color.parseColor("#133B5C") // Replace with your desired color
         }
+
+        // initializing our variable for textView with its id.
+        val tvSignUp = findViewById<TextView>(R.id.signUp_link)
+        tvSignUp.setOnClickListener {
+
+            // on below line we are creating a new bottom sheet dialog.
+            val dialog = BottomSheetDialog(this)
+
+            // on below line we are inflating a layout file which we have created.
+            val view = layoutInflater.inflate(R.layout.signup_bottomsheet_layout, null)
+
+            // below line is use to set cancelable to avoid
+            // closing of dialog box when clicking on the screen.
+            dialog.setCancelable(true)
+
+            // on below line we are setting
+            // content view to our view.
+            dialog.setContentView(view)
+
+            // on below line we are calling
+            // a show method to display a dialog.
+            dialog.show()
+        }
+
+        }
+
     }
-}
