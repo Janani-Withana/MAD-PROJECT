24d4d2c58ab30850a31e613ad2e1f2162442856b
IT21786464 <IT21786464@my.sliit.lk>
2023-03-19 20:26:18 +0530

    all done

--- app/src/main/java/com/example/electricitysaver/adminListAdapter.kt
+++ app/src/main/java/com/example/electricitysaver/adminListAdapter.kt
@@ -13,7 +13,7 @@ class adminListAdapter(private var items: ArrayList<adminList>):
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textView: TextView = itemView.findViewById(R.id.icategory)
         val textView1: TextView = itemView.findViewById(R.id.uhours)
-        val imageView1: ImageView = itemView.findViewById(R.id.editlist)
+        val imageView1: ImageView = itemView.findViewById(R.id.editlistwaren1)
         val imageView2: ImageView = itemView.findViewById(R.id.deletelist)
     }
 
