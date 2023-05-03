894d430b9647d38957de4bb7d8a8d85546cca670
Nipun Wijethunge <it21559136@my.sliit.lk>
2023-05-03 10:11:15 +0530

    added cost history

---
+++ app/src/main/java/com/example/electricitysaver/cost_history.kt
@@ -0,0 +1,11 @@
+package com.example.electricitysaver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cost_history : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cost_history)
+    }
+}
