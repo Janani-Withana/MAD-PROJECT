1a188f2ee88b208a5652f57d114f94db1dc46cd1
Pamitha <89682916+Pamitha-Pankaja@users.noreply.github.com>
2023-05-03 09:48:09 +0530

    add card view and recycler view

--- app/src/main/java/com/example/electricitysaver/pamitha_payment.kt
+++ app/src/main/java/com/example/electricitysaver/pamitha_payment.kt
@@ -1,16 +1,33 @@
 package com.example.electricitysaver
 
+import android.content.ContentValues
+import android.content.DialogInterface
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.SimpleCursorAdapter
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.activity_pamitha_payment2.*
+
 
 class pamitha_payment : AppCompatActivity() {
+
+    lateinit var db: SQLiteDatabase
+    lateinit var rs: Cursor
+    lateinit var adapter: SimpleCursorAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pamitha_payment2)
+
+        var helper = paymentHelper(applicationContext)
+        db = helper.readableDatabase
 
         supportActionBar?.hide()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -33,6 +50,23 @@ class pamitha_payment : AppCompatActivity() {
 
             // on below line we are calling a show method to display a dialog.
             dialog.show()
+        }
+
+        AddFav.setOnClickListener {
+            var cv = ContentValues()
+            cv.put("PAYEE",payeeCat.selectedItem.toString())
+            cv.put("NAME",ptAccountName.text.toString())
+            cv.put("ACCOUNT",ActNumber.text.toString())
+            cv.put("AMOUNT",etAmount.text.toString())
+            db.insert("PAYMENT",null,cv)
+
+            var ad = AlertDialog.Builder(this)
+            ad.setTitle("Add To Favourite")
+            ad.setMessage("Record Added to Favourite List Successflly ....!")
+            ad.setPositiveButton("OK", DialogInterface.OnClickListener{ dialog: DialogInterface?, i->
+            })
+            ad.show()
+
         }
     }
 }
