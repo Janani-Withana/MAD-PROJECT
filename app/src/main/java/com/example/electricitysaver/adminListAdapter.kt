2e69ca4cf92c8f6fa7add283d31db472dad545f4
Pamitha <89682916+Pamitha-Pankaja@users.noreply.github.com>
2023-05-04 13:33:42 +0530

    changes updated

--- app/src/main/java/com/example/electricitysaver/adminListAdapter.kt
+++ app/src/main/java/com/example/electricitysaver/adminListAdapter.kt
@@ -7,27 +7,42 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class adminListAdapter(private var items: ArrayList<adminList>):
+class adminListAdapter(private var items: ArrayList<adminList>,private val editListener: EditListener,
+                       private val deleteListener: DeleteListener):
     RecyclerView.Adapter<adminListAdapter.ViewHolder>(){
 
+    interface EditListener {
+        fun onEditClicked(item: adminList)
+    }
+    interface DeleteListener {
+        fun onDeleteClicked(item: adminList)
+    }
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textView: TextView = itemView.findViewById(R.id.icategory)
-        val textView1: TextView = itemView.findViewById(R.id.uhours)
-        val imageView1: ImageView = itemView.findViewById(R.id.editlistwaren1)
-        val imageView2: ImageView = itemView.findViewById(R.id.deletelist)
+        val textView: TextView = itemView.findViewById(R.id.category)
+        val textView1: TextView = itemView.findViewById(R.id.brand)
+        val textView2: TextView = itemView.findViewById(R.id.watts)
+        val imageView1: ImageView = itemView.findViewById(R.id.editItem)
+        val imageView2: ImageView = itemView.findViewById(R.id.deleteItem)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_recycler_view_layout, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.admin_list_recycler_view, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
-        holder.textView.text = item.brand //data class variables
-        holder.textView1.text = item.watt.toString() //data class variables
+        holder.textView.text = item.category //data class variables
+        holder.textView1.text = item.brand //data class variables
+        holder.textView2.text = item.watt.toString() //data class variables
         holder.imageView1.setImageResource(R.drawable.ic_baseline_edit_note_blue)
+        holder.imageView1.setOnClickListener {
+            editListener.onEditClicked(item)
+        }
         holder.imageView2.setImageResource(R.drawable.ic_baseline_delete_outline_24)
+        holder.imageView2.setOnClickListener {
+            deleteListener.onDeleteClicked(item)
+        }
     }
 
     override fun getItemCount(): Int {
