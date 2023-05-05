772f8cc03a0728b31e0f76dc637adcf6428a3581
Nipun Wijethunge <it21559136@my.sliit.lk>
2023-05-05 15:47:49 +0530

    edited cost calculator

--- app/src/main/java/com/example/electricitysaver/cost_history.kt
+++ app/src/main/java/com/example/electricitysaver/cost_history.kt
@@ -1,25 +1,16 @@
 package com.example.electricitysaver
 
-import android.content.Intent
-import android.graphics.Color
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class cost_history : AppCompatActivity() {
+    private lateinit var startDateEditText: EditText
+    private lateinit var endDateEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cost_history)
-
-        supportActionBar?.hide()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.navigationBarColor =
-                Color.parseColor("#133B5C") // Replace with your desired color
-        }
-
     }
-
 
 }
