2f9d4e1d5e97eacabc1c25d609aae9a6bf14ae6e
IT21786464 <IT21786464@my.sliit.lk>
2023-05-14 20:22:38 +0530

    unit testing and instrumental testing done ui updatde and alert box added and add form validation

--- app/src/main/java/com/example/electricitysaver/User_addItems.kt
+++ app/src/main/java/com/example/electricitysaver/User_addItems.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.electricitysaver.databaseHelper.UserItemDbHelper
 
@@ -45,37 +46,65 @@ class User_addItems : AppCompatActivity() {
         nw = findViewById(R.id.nw)
 
         btnCusAdd.setOnClickListener{
-            var cv = ContentValues()
-            cv.put("CATEGORY",ccat.text.toString())
-            cv.put("BRAND",brand.text.toString())
-            cv.put("QTY",qty.text.toString().toInt())
-            cv.put("DAYUSE",du.text.toString().toInt())
-            cv.put("MONUSE",mu.text.toString().toInt())
-            cv.put("NOW",nw.text.toString().toInt())
-            db.insert("USER_ADD_ITEM",null,cv)
+            val category = ccat.text.toString().trim()
+            val brandName = brand.text.toString().trim()
+            val quantity = qty.text.toString().trim()
+            val dayUse = du.text.toString().trim()
+            val monthUse = mu.text.toString().trim()
+            val nowValue = nw.text.toString().trim()
+
+            // Validate input fields
+            if (category.isEmpty() || brandName.isEmpty() || quantity.isEmpty() || dayUse.isEmpty() || monthUse.isEmpty() || nowValue.isEmpty()) {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (quantity.toInt() <= 0 ) {
+                qty.error = "Quantity must be greater than 0"
+                return@setOnClickListener
+            }
+            if(dayUse.toInt() <= 0 || dayUse.toInt() > 24){
+                du.error = "Day use must be greater than 0 and less than 24"
+                return@setOnClickListener
+            }
+            if(monthUse.toInt() <= 0 || monthUse.toInt() > 720){
+                mu.error = "Month use must be greater than 0 and less than 720"
+                return@setOnClickListener
+            }
+
+            val cv = ContentValues().apply {
+                put("CATEGORY", category)
+                put("BRAND", brandName)
+                put("QTY", quantity.toInt())
+                put("DAYUSE", dayUse.toInt())
+                put("MONUSE", monthUse.toInt())
+                put("NOW", nowValue.toInt())
+            }
+            val rowsAffected = db.insert("USER_ADD_ITEM", null, cv)
             rs.requery()
 
-            //Show pop up box
-            var ad = AlertDialog.Builder(this)
-            ad.setTitle("Add record")
-            ad.setMessage("Record Inserted Successfully....!")
-            ad.setPositiveButton("OK", DialogInterface.OnClickListener{ DialogInterface, i ->
-                ccat.setText("")
-                brand.setText("")
-                brand.setText("")
-                qty.setText("")
-                du.setText("")
-                mu.setText("")
-                nw.setText("")
+            if(rowsAffected > 0 ) {
+                //Show pop up box
+                val ad = AlertDialog.Builder(this)
+                ad.setTitle("Add record")
+                ad.setMessage("Record Inserted Successfully....!")
+                ad.setPositiveButton("OK") { DialogInterface, i ->
+                    ccat.setText("")
+                    brand.setText("")
+                    brand.setText("")
+                    qty.setText("")
+                    du.setText("")
+                    mu.setText("")
+                    nw.setText("")
+                    ccat.requestFocus()
+                }
+                ad.show()
 
-                ccat.requestFocus()
-            })
-            ad.show()
+            }else{
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+            }
+
 
         }
-
-
-
 
         val buttonList = findViewById<Button>(R.id.btnlist)
         buttonList.setOnClickListener {
